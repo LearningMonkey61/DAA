19561f8a7767fa21f482ac67c0dd015f02d3cb25
LearningMonkey61 <b118061@iiit-bh.ac.in>
2020-03-02 15:19:18 +0530

    Nearly Sorted in O(lg n)

--- nearlysorted.cpp
+++ nearlysorted.cpp
@@ -3,8 +3,40 @@ using namespace std;
 #define lli long long int
 #define MOD 1000000007
 
+int nbSearch(int a[],int start , int end , int item){
+    int mid = (start + end) / 2 ;
+
+    if(a[mid+1] == item || a[mid-1] == item){
+        return (a[mid+1] == item) ? mid + 2 : mid ;   
+    }
+    else{
+        if( start == end){
+            if(a[mid] == item){
+                return mid + 1 ; 
+            }
+            else{
+                return -1 ;
+            }
+        } 
+        else if(item <= a[mid]){
+            return nbSearch(a,start,mid,item);
+        }
+        else{
+            return nbSearch(a,mid+1,end,item); 
+        }
+    }
+}
+
 void CowboyBebop(){
-    
+    int n ; 
+    cin >> n ; 
+    int a[n] ; 
+    for(int i = 0 ; i < n ; i++){
+        cin >> a[i] ; 
+    }
+    int item ; 
+    cin >> item ; 
+    cout << nbSearch(a,0,n,item) << endl  ;
 }
 
 using namespace std;
@@ -14,8 +46,8 @@ int main(void){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
 
-    int t ;
-    cin >> t;
+    int t = 1  ;
+    // cin >> t;
 
     while(t--){
         CowboyBebop();
